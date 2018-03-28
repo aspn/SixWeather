650c967488c145926bf55f7730d7670618483bc1
Anson Poon <anson.poon@rubikloud.com>
2018-03-28 16:34:32 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    const DARK_SKY_KEY = '166e86bf5945e1c57eaf06dffaffaff5';
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log('GETTING POSITION');
+        console.log(position);
+        const darkSkyWeather = getDarkSkyWeather(position.coords.latitude, position.coords.longitude);
+        darkSkyWeather.then((success) => {
+          console.log('WEATHER SUCCESS: ', success);
+        });
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+
+      },
+      (error) => this.setState({
+        error: error.message
+      }), {
+        enableHighAccuracy: false,
+        timeout: 200000,
+        maximumAge: 1000
+      },
+    );
+
+    async function getDarkSkyWeather(lat, long) {
+      try {
+        let response = await fetch(
+          `https://api.darksky.net/forecast/${DARK_SKY_KEY}/${lat},${long}?units=ca`
+        );
+        let responseJson = await response.json();
+        return responseJson;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titleText}>6 weather</Text>
+        <Text style={styles.locationText}> {this.state.latitude} </Text>
+        <Text style={styles.locationText}> {this.state.longitude} </Text>
+        <Text style={styles.locationText}> {this.state.error} </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+    fontFamily: 'Futura',
+    fontWeight: '700',
+    fontSize: 50,
+    letterSpacing: -0.6,
+    color: '#A1917C',
+  },
+  locationText: {
+    fontFamily: 'Futura',
+    fontWeight: '100',
+    fontSize: 20,
+    letterSpacing: -0.6,
+    color: '#A1917C',
+  },
+});
